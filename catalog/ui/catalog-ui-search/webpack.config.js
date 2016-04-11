c30d6e9259b438ba6dba733c8c06b0ab24afdd89
Andrew Fiedler <andrewkfiedler@gmail.com>
2016-04-11 15:51:06 -0700

    Adds webpack config

---
+++ catalog/ui/catalog-ui-search/webpack.config.js
@@ -0,0 +1,102 @@
+var path = require('path');
+
+var resolve = function (place) {
+    return path.resolve(__dirname, place);
+}
+
+module.exports = {
+    context: resolve('./src/main/webapp/js'),
+    entry: resolve('./src/main/webapp/main.js'),
+    output: {
+        path: resolve('./target/webapp'),
+        filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: './target/webapp/',
+        proxy: {
+            '/services/*': {
+                target: 'https://localhost:8993',
+                secure: false
+            }
+        }
+    },
+    devtool: 'eval',
+    module: {
+        loaders: [
+            {test: /^text/, loader: 'text'}
+        ]
+    },
+    resolve: {
+        alias: {
+            bootstrap: 'components-bootstrap/js/bootstrap.min',
+            bootstrapselect: 'bootstrap-select/dist/js/bootstrap-select.min',
+            cometd$: 'cometd/org/cometd',
+            bootstrapDatepicker: 'eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min',
+            jquerycometd: 'cometd/jquery/jquery.cometd',
+            moment: 'moment/min/moment.min',
+            perfectscrollbar: 'perfect-scrollbar/min/perfect-scrollbar.min',
+            spin: 'spin.js/spin',
+            q: 'q/q',
+            strapdown: 'strapdown/v/0.2',
+            spectrum: 'spectrum/spectrum',
+            // backbone
+            backbone: 'components-backbone/backbone',
+            backboneassociations: 'backbone-associations/backbone-associations',
+            backbonecometd: 'backbone-cometd/backbone.cometd.extension',
+            backboneundo: 'Backbone.Undo.js/Backbone.Undo',
+            poller: 'backbone-poller/backbone.poller',
+            underscore: 'lodash/lodash',
+            marionette: 'marionette/lib/backbone.marionette',
+            // TODO test combining
+            modelbinder: 'backbone.modelbinder/Backbone.ModelBinder',
+            collectionbinder: 'backbone.modelbinder/Backbone.CollectionBinder',
+            // application
+            application: 'js/application',
+            cometdinit: 'js/cometd',
+            direction: 'js/direction',
+            webglcheck: 'js/webglcheck',
+            twodcheck: 'js/2dmapcheck',
+            maptype: 'js/maptype',
+            spinnerConfig: 'js/spinnerConfig',
+            wreqr: 'js/wreqr',
+            properties: 'properties',
+            // jquery
+            jquery$: 'jquery/jquery',
+            jqueryCookie: 'jquery-cookie/jquery.cookie',
+            jqueryuiCore: 'jquery-ui/ui/minified/jquery.ui.core.min',
+            datepicker: 'jquery-ui/ui/minified/jquery.ui.datepicker.min',
+            progressbar: 'jquery-ui/ui/minified/jquery.ui.progressbar.min',
+            slider: 'jquery-ui/ui/minified/jquery.ui.slider.min',
+            mouse: 'jquery-ui/ui/minified/jquery.ui.mouse.min',
+            datepickerOverride: 'jquery/js/plugin/jquery-ui-datepicker-4digitYearOverride-addon',
+            purl: 'purl/purl',
+            multiselect$: 'multiselect/src/jquery.multiselect',
+            multiselectfilter: 'multiselect/src/jquery.multiselect.filter',
+            'jquery.ui.widget': 'jquery-ui/ui/minified/jquery.ui.widget.min',
+            fileupload: 'jquery-file-upload/js/jquery.fileupload',
+            jquerySortable: 'jquery-ui/ui/minified/jquery.ui.sortable.min',
+            // handlebars
+            handlebars: 'handlebars/handlebars.min',
+            // require plugins
+            text: 'requirejs-plugins/lib/text',
+            css: 'require-css/css.min',
+            // pnotify
+            pnotify: 'pnotify/jquery.pnotify.min',
+            // map
+            //cesium: 'cesiumjs/Cesium/Cesium',
+            cesium: 'cesium/Build/CesiumUnminified/Cesium.js',
+            drawHelper: 'cesium-drawhelper/DrawHelper',
+            //openlayers: 'openlayers3/build/ol',
+            usngs: 'usng.js/usng',
+            wellknown: 'wellknown/wellknown'
+        },
+        root: [
+            './src/main/webapp/',
+            './src/main/webapp/js',
+            './src/main/webapp/lib/',
+            './target/webapp/lib',
+            './node_modules'
+        ].map(resolve)
+    }
+
+}
